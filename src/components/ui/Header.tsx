861e906f600bb699b8930c8a9fa80786596a1cbb
Grigory Morgachev <grinmorg@gmail.com>
2023-11-24 21:22:14 +0300

    Добавил адаптацию для Главной и ссылку для лого в шапке

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,12 +1,15 @@
 import { Button } from 'antd';
 import Logo from '../../assets/icons/Logo';
 import LoginIcon from '../../assets/icons/LoginIcon';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="mx-auto my-11 flex max-w-[90rem] items-center justify-between text-3xl">
-      <Logo />
-      <Button className="mx-8  my-[12.5px] flex h-12 w-40 items-center justify-center gap-2 rounded-full border-none bg-amber-200 text-lg font-bold hover:text-gray-950">
+    <header className="mx-4 my-4 flex max-w-[90rem] items-center justify-between text-3xl md:my-11 xl:mx-auto">
+      <Link to="/">
+        <Logo />
+      </Link>
+      <Button className="my-[12.5px] flex h-12 w-40 items-center justify-center gap-2 rounded-full border-none bg-amber-200 text-lg font-bold hover:text-gray-950 md:mx-8">
         Войти
         <LoginIcon />
       </Button>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,10 +4,12 @@ import skyscrapers from '../assets/images/skyscrapers.jpg';
 function MainPage() {
   return (
     <div className="mx-auto my-0 max-w-[90rem]">
-      <h1 className="mx-32 mb-3 mt-6">Улучшаем город вместе</h1>
-      <p className="mx-32 mb-[52px] text-lg">Портал ЖКХ Екатеринбурга</p>
-      <div className="mx-32 mb-[100px] grid max-w-[74rem] grid-cols-3 gap-5">
-        <div className="row-span-2 h-[644px] w-[381px] rounded-2xl">
+      <h1 className="mx-4 mb-3 mt-6 md:mx-32">Улучшаем город вместе</h1>
+      <p className="mx-4 mb-[52px] text-lg md:mx-32">
+        Портал ЖКХ Екатеринбурга
+      </p>
+      <div className="mx-4 mb-[100px] grid max-w-[74rem] grid-cols-1 gap-5 md:mx-32 md:grid-cols-2 xl:grid-cols-3">
+        <div className="row-span-2 rounded-2xl xl:h-[644px]">
           <div className=" h-[312px] rounded-t-2xl bg-blue-100 pl-8 pr-[83px] pt-8">
             <h3 className="mb-4">Карта плановых работ</h3>
             <p>
@@ -16,26 +18,26 @@ function MainPage() {
             </p>
           </div>
           <img
-            className="rounded-b-2xl"
+            className="w-full rounded-b-2xl"
             src={map}
             alt="Карта Екатеринбурга"
           ></img>
         </div>
-        <div className="h-[312px] w-[382px] rounded-2xl bg-violet-100 pl-8 pr-[83px] pt-8">
+        <div className="h-[312px] rounded-2xl bg-violet-100 pl-8 pr-[83px] pt-8">
           <h3 className="mb-4">Новости</h3>
           <p>
             Показываем, где сейчас проходят плановые работы по улучшению нашего
             города. Отмечаем важные места и мероприятия.
           </p>
         </div>
-        <div className="flex h-[312px] w-[381px] items-end">
+        <div className="flex items-end xl:h-[312px]">
           <img
-            className="rounded-b-2xl"
+            className="h-full w-full rounded-b-2xl"
             src={skyscrapers}
             alt="Изображение небоскребов"
           ></img>
         </div>
-        <div className="col-span-2 h-[312px] w-[783px] rounded-2xl bg-amber-100 py-8 pl-8">
+        <div className="h-[312px] rounded-2xl bg-amber-100 py-8 pl-8 md:col-span-2">
           <h3 className="mb-4">Заявки</h3>
           <p>
             Подать обращение с жалобой или предложением, история заявок,
